4ccb53116966cfb4f9ae2688484ba9b10c0ffc88
hebertchaves <hebert.chaves@veolia.com>
2025-04-22 23:39:53 -0300

    v5 - fase 1.9.4
    aplicando classes default do quasar

--- rollup.config.js
+++ rollup.config.js
@@ -1,8 +1,9 @@
-// rollup.config.js
+// Modifique o arquivo rollup.config.js
 import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 import typescript from '@rollup/plugin-typescript';
 import { terser } from 'rollup-plugin-terser';
+import copy from 'rollup-plugin-copy'; // Você pode precisar instalar este pacote: npm install rollup-plugin-copy --save-dev
 
 export default {
   input: 'src/code.ts',
@@ -17,6 +18,12 @@ export default {
     typescript({
       tsconfig: './tsconfig.json',
       sourceMap: false
+    }),
+    // Adicione este plugin para copiar o arquivo HTML
+    copy({
+      targets: [
+        { src: 'src/ui.html', dest: 'dist' }
+      ]
     }),
     process.env.NODE_ENV === 'production' && terser()
   ]
